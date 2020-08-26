e05079fa9140dfcff08440c89d5195c94264431f
it-user2 <workerok@yandex.ru>
2020-08-26 09:17:19 +0300

    Updates

---
+++ precache-manifest.be9408882acb15065ead12db68e95e62.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8730e223f7701a5b2383c39423d46007",
+    "url": "/react-kabzda-1/index.html"
+  },
+  {
+    "revision": "7ff193e3f9a340c87d7c",
+    "url": "/react-kabzda-1/static/css/3.35f768e0.chunk.css"
+  },
+  {
+    "revision": "80141a4c6416904fe107",
+    "url": "/react-kabzda-1/static/css/4.c7cb900c.chunk.css"
+  },
+  {
+    "revision": "49f03c331e14123ea276",
+    "url": "/react-kabzda-1/static/css/main.0e553eff.chunk.css"
+  },
+  {
+    "revision": "dbcfee0fa22d322094f5",
+    "url": "/react-kabzda-1/static/js/2.ba52be2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-kabzda-1/static/js/2.ba52be2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ff193e3f9a340c87d7c",
+    "url": "/react-kabzda-1/static/js/3.a27330a6.chunk.js"
+  },
+  {
+    "revision": "80141a4c6416904fe107",
+    "url": "/react-kabzda-1/static/js/4.52b114b8.chunk.js"
+  },
+  {
+    "revision": "49f03c331e14123ea276",
+    "url": "/react-kabzda-1/static/js/main.8e546172.chunk.js"
+  },
+  {
+    "revision": "1bba41bc07625bed0185",
+    "url": "/react-kabzda-1/static/js/runtime-main.d55f968c.js"
+  },
+  {
+    "revision": "3f32cfe83ece87c73a7f13669de59f85",
+    "url": "/react-kabzda-1/static/media/lookforajob.3f32cfe8.png"
+  },
+  {
+    "revision": "bf9ea68109dafaac18c04ce1bc0ebb5b",
+    "url": "/react-kabzda-1/static/media/looknotforajob.bf9ea681.png"
+  },
+  {
+    "revision": "c09926edc8442ba6faf2f7dadacececd",
+    "url": "/react-kabzda-1/static/media/preloader.c09926ed.gif"
+  }
+]);
